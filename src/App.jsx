9ca9bbb4c070816ddfb7757d1fa278fb4a42b8e0
Xtina1110 <cr.alvarez.souto@gmail.com>
2025-06-25 16:05:07 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import PantallaCarga from './components/PantallaCarga';
 import PantallaInicio from './components/PantallaInicio';
 import Login from './components/Login';
 import Terminos from './components/Terminos';
+import Privacidad from './components/Privacidad';
 
 function App() {
   return (
@@ -13,10 +15,10 @@ function App() {
         <Route path="/inicio" element={<PantallaInicio />} />
         <Route path="/login" element={<Login />} />
         <Route path="/terminos" element={<Terminos />} />
+        <Route path="/privacidad" element={<Privacidad />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
