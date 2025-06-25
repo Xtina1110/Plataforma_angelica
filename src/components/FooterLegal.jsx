37a4de7f3c3bc86691eae9ec6b8882fa87ff6f80
Xtina1110 <cr.alvarez.souto@gmail.com>
2025-06-25 15:50:20 -0400

    Update FooterLegal.jsx

--- src/components/FooterLegal.jsx
+++ src/components/FooterLegal.jsx
@@ -10,13 +10,13 @@ const FooterLegal = () => {
       </div>
       <div className="mt-2 flex justify-center gap-4">
         <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-          <img src="/icons/facebook.svg" alt="Facebook" className="w-5 h-5 inline" />
+          <img src="/icons/facebook.jpg" alt="Facebook" className="w-5 h-5 inline" />
         </a>
         <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-          <img src="/icons/instagram.svg" alt="Instagram" className="w-5 h-5 inline" />
+          <img src="/icons/IG.png" alt="Instagram" className="w-5 h-5 inline" />
         </a>
         <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
-          <img src="/icons/youtube.svg" alt="YouTube" className="w-5 h-5 inline" />
+          <img src="/icons/Youtube.jpg" alt="YouTube" className="w-5 h-5 inline" />
         </a>
       </div>
       <p className="mt-2 text-xs text-gray-400">© 2025 Plataforma Angélica</p>
