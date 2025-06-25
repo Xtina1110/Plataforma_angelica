4fa1f0d538085700084e7ce2a1709b26320dac21
Xtina1110 <cr.alvarez.souto@gmail.com>
2025-06-25 15:14:28 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import PantallaCarga from './components/PantallaCarga';
+import PantallaInicio from './components/PantallaInicio';
 import './App.css';
 
 function App() {
-  return <PantallaCarga />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<PantallaCarga />} />
+        <Route path="/inicio" element={<PantallaInicio />} />
+        {/* Puedes agregar más pantallas aquí luego, como login, registro, dashboard, etc */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
