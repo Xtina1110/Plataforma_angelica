623ff04f9cd236a6ea40a69976287dba6ac68244
Xtina1110 <cr.alvarez.souto@gmail.com>
2025-06-25 12:06:26 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PantallaCarga from './components/PantallaCarga';
+
+function App() {
+  return <PantallaCarga />;
+}
+
+export default App;
---
+++ src/components/PantallaCarga.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PantallaCarga = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-yellow-100 to-white text-center">
+      <h1 className="text-4xl font-bold text-yellow-700">
+        Plataforma Angélica funcionando 🌟
+      </h1>
+    </div>
+  );
+};
+
+export default PantallaCarga;
