2ac2913665c011698daf1114f56e117e4608c73b
Xtina1110 <cr.alvarez.souto@gmail.com>
2025-06-26 09:22:08 -0400

    Create Dashboard.jsx

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+// src/components/Dashboard.jsx
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-yellow-600">Dashboard de Usuario</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
