c5abb964c4f50cbb09398ced10255e5b391c6a50
Xtina1110 <cr.alvarez.souto@gmail.com>
2025-06-27 09:59:38 -0400

    Create paises.js

---
+++ src/data/paises.js
@@ -0,0 +1,9 @@
+// src/components/data/paises.js
+export const paises = [
+  "Afganistán", "Alemania", "Argentina", "Australia", "Bolivia", "Brasil",
+  "Canadá", "Chile", "China", "Colombia", "Costa Rica", "Cuba", "Ecuador",
+  "Egipto", "El Salvador", "España", "Estados Unidos", "Francia", "Guatemala",
+  "Honduras", "India", "Italia", "Japón", "México", "Nicaragua", "Panamá",
+  "Paraguay", "Perú", "Portugal", "Reino Unido", "República Dominicana",
+  "Rusia", "Sudáfrica", "Uruguay", "Venezuela"
+];
