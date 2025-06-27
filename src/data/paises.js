937aa307d560b0c887e0f06d85d1f2edb89acbea
Xtina1110 <cr.alvarez.souto@gmail.com>
2025-06-27 10:17:22 -0400

    Update paises.js

--- src/data/paises.js
+++ src/data/paises.js
@@ -1,9 +1,14 @@
 // src/components/data/paises.js
 export const paises = [
-  "Afganistán", "Alemania", "Argentina", "Australia", "Bolivia", "Brasil",
-  "Canadá", "Chile", "China", "Colombia", "Costa Rica", "Cuba", "Ecuador",
-  "Egipto", "El Salvador", "España", "Estados Unidos", "Francia", "Guatemala",
-  "Honduras", "India", "Italia", "Japón", "México", "Nicaragua", "Panamá",
-  "Paraguay", "Perú", "Portugal", "Reino Unido", "República Dominicana",
-  "Rusia", "Sudáfrica", "Uruguay", "Venezuela"
+  "Afganistán", "Alemania", "Argentina", "Australia", "Austria", "Bélgica",
+  "Bolivia", "Brasil", "Canadá", "Chile", "China", "Colombia", "Corea del Sur",
+  "Costa Rica", "Cuba", "Dinamarca", "Ecuador", "Egipto", "El Salvador",
+  "Emiratos Árabes Unidos", "España", "Estados Unidos", "Estonia", "Filipinas",
+  "Finlandia", "Francia", "Grecia", "Guatemala", "Honduras", "Hungría",
+  "India", "Indonesia", "Irlanda", "Israel", "Italia", "Japón", "Letonia",
+  "Lituania", "Malasia", "México", "Nicaragua", "Noruega", "Nueva Zelanda",
+  "Países Bajos", "Panamá", "Paraguay", "Perú", "Polonia", "Portugal",
+  "Puerto Rico", "Reino Unido", "República Checa", "República Dominicana",
+  "Rumania", "Rusia", "Singapur", "Sudáfrica", "Suecia", "Suiza", "Tailandia",
+  "Turquía", "Ucrania", "Uruguay", "Venezuela", "Vietnam"
 ];
