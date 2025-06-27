2a60771ccbe204bfbddf5a3cec12308a621033b3
Xtina1110 <cr.alvarez.souto@gmail.com>
2025-06-27 19:10:05 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
+// 📁 src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css'; // Asegúrate de tener Tailwind configurado aquí
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// Archivos de estilos
+import './App.css';
+import './index.css';
+import './PantallaCarga.css';
+import './PantallaInicio.css';
+import './PantallaLogin.css';
+import './PantallaRegistro.css';
+import './PantallaDashboard.css';
+import './PantallaTiradaAngelica.css';
+
+// Renderizar la app
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
